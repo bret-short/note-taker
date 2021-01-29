2c92adea69a01b8a776e98fe99ab729e1ad452d4
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2021-01-29 16:26:31 -0700

    setting up the get routes

--- server.js
+++ server.js
@@ -14,3 +14,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// Routes
+// =============================================================
+
+// Basic route that sends the user to the index page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// Basic route that sends the user to the notes page
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// Route that sends the user the db.json file
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "db/db.json"));
+});
+
